b2ce8afa8b1056310a765445a2e999c111ae15ea
Himindu-Kularathne <himindu.21@cse.mrt.ac.lk>
2024-09-12 11:18:23 +0530

    make the docker ignore files

--- server/config/dbconnection.js
+++ server/config/dbconnection.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host: process.env.MYSQL_HOST || 'db',
+    host: process.env.MYSQL_HOST || 'localhost',
     user: process.env.MYSQL_USER || 'root',
-    password: process.env.MYSQL_PASSWORD || 'password',
+    password: process.env.MYSQL_ROOT_PASSWORD || 'password',
     database: process.env. MYSQL_DATABASE || 'cmsystem',
+    port: 3306
 });
 
 db.connect((err) => {
